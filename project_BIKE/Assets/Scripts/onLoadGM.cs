f3f567a4e8dbe8b5dcc2632f3f268a1106085c62
DESKTOP-O10JD7J\Alec <alec.burmeister@gmail.com>
2019-12-12 18:29:28 -0600

    Initial commit
    - Added test players to get animated characters to be able to switch between them
    - scripts added to change the animation, and to save the last player used.

---
+++ project_BIKE/Assets/Scripts/onLoadGM.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class onLoadGM : MonoBehaviour
+{
+	private Animator currController;
+
+	public GameObject player;
+
+	void Awake() {
+		// TODO: When the controller for the animation (chosen player) is fixed/completed inside of changePlayer.cs, 
+		// make sure to add it here so that the player loads up as the last player it played as.
+		currController = player.GetComponent<Animator>();
+		currController.runtimeAnimatorController = Resources.Load("Animations/player" + PlayerPrefs.GetInt("currPlayer", 0) + "_0") as RuntimeAnimatorController;
+  
+	}
+
+}
